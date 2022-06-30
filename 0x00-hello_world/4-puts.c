1f6e90a8ea03aa823063faf63148fac063b89f69
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2022-06-30 12:44:29 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
-
 /**
- *
- * * main - Write '"Programming is like building a multilingual puzzle'
- *
- * *Return: Always 0 (success)
- *
- * */
-
+* main - Write '"Programming is like building a multilingual puzzle'
+*Return: Always 0 (success)
+*/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
