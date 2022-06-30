7d6bdb930445ff59d515114dbc73ae1391b3eda9
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-06-30 04:38:31 -0700

    C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *
+ * * main - Write '"Programming is like building a multilingual puzzle'
+ *
+ * *Return: Always 0 (success)
+ *
+ * */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
