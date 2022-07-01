06b6cf7c7c45a0c3fdfb2895fcb46a65c46a3130
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2022-07-01 17:07:56 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,39 +1,20 @@
 #include <stdio.h>
-
 /**
- *
- *  * main - main block
- *
- *   * Description: prints all single digit numbers of base 10
- *
- *    * starting from 0, followed by a new line.
- *
- *     * Return: 0
- *
- *      */
-
+ * main - main block
+ * Description: prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ * Return: 0
+ */
 int main(void)
-
 {
+	int c = 0;
 
-		int c = 0;
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
 
-
-
-			while (c < 10)
-
-					{
-
-								putchar(48 + c);
-
-										c++;
-
-											}
-
-
-
-				putchar('\n');
-
-					return (0);
-
+	putchar('\n');
+	return (0);
 }
