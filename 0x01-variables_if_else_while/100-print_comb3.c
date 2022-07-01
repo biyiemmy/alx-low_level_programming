d4138fb7742138d92ec996b2a6d60723323836e8
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2022-07-01 18:52:47 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,47 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Numbers must be separated by ,, followed by a space
+ * main - main block to be executed
+ * Description: write program that prints all the combinations of two digits
+ * Numbers to be separated with ,, followed by a space
  * The two digits must be different
- * 01 and 10 are considered the same combination of the two digits 0 and 1
- * Print only the smallest combination of two digits
- * Numbers should be printed in ascending order, with two digits
- * You can only use the putchar function
- * (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar five times maximum in your code
+ * 01 and 10 are considered the same combination of two digits
+ * Print just the smallest combination of two digits
+ * Numbers should be printed in ascending order
+ * Putchar is the only function to be used
+ * all other functions are forbidden( printf, puts, etc..)
+ * Putchar to be use five times maximum
  * You are not allowed to use any variable of type char
- * All your code should be in the main function
- * Return: 0
+ * all code should be in main function
+ *Return: 0 Success
  */
 int main(void)
 {
-	int c;
-	int d = 0;
-
-	while (d < 10)
-	{
-		c = 0;
-		while (c < 10)
-		{
-			if (d != c && d < c)
-			{
-				putchar('0' + d);
-				putchar('0' + c);
-
-				if (c + d != 17)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-
-			c++;
-		}
-		d++;
-	}
-	putchar('\n');
-	return (0);
+    int c;
+    int d = 0;
+    while (d < 10)
+    {
+        c = 0;
+        while (c < 10)
+        {
+            if (d != c && d < c)
+            {
+                putchar('0' + d);
+                putchar('0' + c);
+                if (c + d != 17)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+            c++;
+        }
+        d++;
+    }
+    putchar('\n');
+    return (0);
 }
