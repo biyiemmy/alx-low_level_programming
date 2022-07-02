d44c840b53a432f7d87d7ac0a2919ea318a7da97
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-02 04:14:56 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,21 +3,22 @@
 /**
  * main - block to be executed
  * Description: print all single digits
- * Return: 0 Success
+ * Return : 0 Success
  */
 int main(void)
 {
 	int c = 0;
+
 	while (c < 10)
 	{
 		putchar(48 + c);
 		if (c != 9)
 		{
-			putchar(',');
-			putchar(' ');
+			putchar(",");
+			putchar(" ");
 		}
 		c++;
 	}
-	putchar('\n');
+	putchar("\n");
 	return (0);
 }
