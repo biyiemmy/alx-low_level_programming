c47e313848d6db50ba9ffaf1e069ed512059830b
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-05 11:21:55 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,7 +35,7 @@ void print_times_table(int n)
 			{
 				_putchar('0' + rep / 100);
 				_putchar('0' + (rep - 100) / 10);
-				_putchar('0' + (rep - 100) / 10);
+				_putchar('0' + rep % 10);
 			}
 			if (b < n)
 			{
