187690dabc554be27d60ce96040445117af4007a
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-05 07:08:46 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,9 @@
 #include "main.h"
-/*
- * main - print putchar
- * Description: Write a program that prints _putchar, followed by a new line
- * Returns: always 0
+
+/**
+ * main - main block
+ * Description: print _putchar followed on a new line
+ * Return: always 0
  */
 int main(void)
 {
