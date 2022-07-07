c4340aad046d043078ae2cce53271cf0ef09cb07
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-07 04:18:18 -0700

    task )

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _is upper -> this is a function that checks for uppercase character
+ * @c: an integer parameter
+ *
+ * Return: something but success
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This is for 0x04. C - More functions, more nested loops project
