534bdcc9a69cc0e1cc8bfdb35c01b4096a487107
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-12 07:48:34 -0700

    task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function that update the reference of a pointer
+ * @n: pointer to n to be updated
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
