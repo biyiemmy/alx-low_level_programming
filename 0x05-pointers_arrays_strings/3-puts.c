1e07c95473324c6dc647520fdca30516399b193c
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-12 08:04:21 -0700

    task 3 done

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - prints str followed by a new line
+ * @str: pointer to the string to point
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i =0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+
+	}
+	_putchar('\n');
+}
