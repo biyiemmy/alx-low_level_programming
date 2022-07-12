f0d2cb89d562bb37d52ed558777a5cdd4387f619
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-12 12:37:50 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - prints reversed string
+ * @s: pointer
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len, i, half;
+	char temp;
+
+	for (len = 0; s[len] != '\0'; len++);
+	
+	i = 0;
+
+	half = len / 2;
+
+	while (half--)
+	{
+		temp = s[len - i - 1];
+		s[len - i - 1] = s[i];
+		s[i] = temp;
+		i++;
+	}
+}
