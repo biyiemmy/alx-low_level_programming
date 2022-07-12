6e4d9c67d54791e8fd68a9c267ea36c5a6fb6005
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-12 09:01:58 -0700

    chn=anges

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print4
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - writes c character in the stdout
+ * @c: its the character to be printed
+ * Return: On success 1
+ * On error, return -1 and set error appropriately
  */
 int _putchar(char c)
 {
