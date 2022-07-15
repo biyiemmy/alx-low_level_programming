f8338bbcfb25463cdf4fbe951b658a2b56b80492
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2022-07-15 12:34:57 +0100

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -15,10 +15,6 @@ int main(void)
 	/**
 	 * write your line of code here...
 	 * Remember:
-	 * - you are not allowed to use a
-	 * - you are not allowed to modify p
-	 * - only one statement
-	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
