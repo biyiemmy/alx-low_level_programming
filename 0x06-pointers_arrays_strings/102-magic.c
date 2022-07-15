d2b98d747cf00b65db1dcd8a8905c0e4627d21b5
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2022-07-15 12:42:40 +0100

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,7 +16,7 @@ int main(void)
 	 * write your line of code here...
 	 * Remember:
 	 */
-	*(p + 5) = 98;
+	p[5] = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
