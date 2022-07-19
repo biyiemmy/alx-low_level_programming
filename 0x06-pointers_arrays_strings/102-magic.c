8e16db5ef3daa2d12cd204b02755ac0ae8f6574b
Adebiyi Emmanuel <74870372+biyiemmy@users.noreply.github.com>
2022-07-19 01:58:24 +0100

    Update 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,12 +16,7 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
- // *(p + 5) = 98;
-
-  printf("%p\n", &a[2]);
-  printf("%p\n", p);
-  for (int i = 0; i < 5; i++)
-	  printf("%p\n", p + (i + 1));
+  *(p + 5) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
   return (0);
