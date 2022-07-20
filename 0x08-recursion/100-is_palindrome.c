8d95aadccc19250ee64df83e253c001f21253588
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-20 06:41:51 -0700

    task 7: update - added block of code for p1

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -15,9 +15,29 @@ int _strlen_recursion(char *s)
 }
 
 /**
- * is_palindrome - palindrome
+ * p1 - palindrome
  * @s: pointer to string
  * @l: position
+ * Return: boolena
+ */
+int p1(char *s, int l)
+{
+	if (l < 1)
+	{
+		return (1);
+	}
+
+	if (*s == *(s + l))
+	{
+		return (p1(s + 1, l - 2));
+	}
+
+	return (0);
+}
+
+/**
+ * is_palindrome - palindrome
+ * @s: pointer to string
  * Return: recursion
  */
 int is_palindrome(char *s)
