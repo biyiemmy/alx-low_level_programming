ad582abf8f9ae57c36680301011522b4c0ca0648
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-07-30 08:14:06 -0700

    task 5 - correction

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,90 +1,97 @@
 #include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
-  * _puts - prints a string
-  * @str: pointer to the string
-  * Return: void
-  */
-
-void _puts(char *str)
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
 {
-	int x = 0;
+	int i = 0;
 
-	while (str[x])
+	while (s[i])
 	{
-		_putchar(str[x]);
-		x++;
+		if (s[i] < '0' || s[i] > '9')
+			return (0);
+		i++;
 	}
+	return (1);
 }
 
 /**
- * _atoi - convert a string to n integer
- * @s: string
- * Return: converted integer
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
  */
-
-int _atoi(const char *s)
+int _strlen(char *s)
 {
-	int X  = 1;
-	unsigned long int Y = 0, myNum, A;
+	int i = 0;
 
-	for (myNum = 0; !(s[myNum] >= 48 && s[myNum] <= 57); myNum++)
+	while (s[i] != '\0')
 	{
-		if (s[myNum] == '-')
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
+	if (!result)
+		return (1);
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			X = X * -1;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-
-	for (A = myNum; s[A] >= 48 && s[A] <= 57; A++)
+	for (i = 0; i < len - 1; i++)
 	{
-		Y = Y * 10;
-		Y = Y + (s[A] - 48);
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
 	}
-
-	return (X * Y);
-}
-
-/**
- * print_int - prints an interger
- * @n: int
- * Return: 0
- */
-
-void print_int(unsigned long int n)
-{
-	unsigned long int Div = 1, B, rep;
-
-	for (B = 0; n / Div > 9; B++)
-		Div = Div * 10;
-
-	for (; Div >= 1; n = n % Div, Div = Div / 10)
-	{
-		rep = n / Div;
-		_putchar('0' + rep);
-	}
-}
-
-/**
- * main - print the result of the multiplication
- * @argc: int
- * @argv: list
- * Return: Always 0
- */
-
-int main(int argc, char const *argv[])
-{
-	(void)argc;
-
-	if (argc != 3)
-	{
-		_puts("Error ");
-		exit(98);
-	}
-	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
-
+	free(result);
 	return (0);
 }
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -10,8 +10,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+int onlyNumbers(char *c);
+void multiply(char* s1, char* s2);
 void errors(void);
 int _strlen(char *s);
-int is_digit(char *s);
+
 
 #endif
