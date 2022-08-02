9743d76c869646828592af228be460f89122a757
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-08-02 07:09:11 -0700

    task 4 - update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ int _strlen(char *str)
 
 /**
  * _strcopy - Copies a string pointed to by src, including the
- * 		terminating null byte, to a buffer pointed to by dest.
+ * terminating null byte, to a buffer pointed to by dest.
  * @dest: The buffer storing the string copy.
  * @src: The source string.
  * Return: The pointer to dest.
