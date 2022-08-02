33acd323016c3dfc7191acf81f3a37ddd6bb35a9
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-08-02 06:31:12 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+
+#define DOG_H
+/**
+ * struct dog - structure
+ * @name: string
+ * @age: float
+ * @owner: characters
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+
+#endif
