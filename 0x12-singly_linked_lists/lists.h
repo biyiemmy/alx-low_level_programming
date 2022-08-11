1262c02057545bd3c5fb384e3cbbcdbd84a4b5df
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-08-11 10:29:47 -0700

    task 5

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,13 +3,12 @@
 #include <stdlib.h>
 
 /**
-* struct list_s - singly linked list
-* @str: string - (malloc'ed string)
-* @len: length of the string
-* @next: points to the next node
-* Description: singly linked list node structure
-*/
-
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ * Description: singly linked list node structure
+ */
 typedef struct list_s
 {
 	char *str;
