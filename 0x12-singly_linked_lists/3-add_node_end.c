1c363e5d99c639c1c714ea4d4ab7049daadc351f
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-08-11 10:34:49 -0700

    task 3: add closing braces

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,11 +3,11 @@
 #include <stdio.h>
 
 /**
-* add_node_end - add a new node at the end of `list_t` list
-* @head: double pointer to head
-* @str: string to duplicate into new node
-* Return: Address of the new element or NULL if failed
-*/
+ * add_node_end - add a new node at the end of `list_t` list
+ * @head: double pointer to head
+ * @str: string to duplicate into new node
+ * Return: Address of the new element or NULL if failed
+ */
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *current;
@@ -37,4 +37,4 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->next = NULL;
 
 	if (current)
-
+}
