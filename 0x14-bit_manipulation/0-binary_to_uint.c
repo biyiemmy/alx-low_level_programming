94029f7aa685dada53e41e56036bfb25061454cb
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-08-18 10:30:12 -0700

    task o:

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,6 +3,7 @@
 /**
  * binary_to_uint - converts a binary number to an unsigned int.
  * @b: pointer to a string containing a binary number
+ *
  * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
 unsigned int binary_to_uint(const char *b)
