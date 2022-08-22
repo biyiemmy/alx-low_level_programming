590222b9a3d30d6c904c6280c6c0ff0727a6a29a
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-08-22 08:18:03 -0700

    task 0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
