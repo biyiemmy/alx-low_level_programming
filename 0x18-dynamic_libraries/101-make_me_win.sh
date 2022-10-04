3a3bd14fc2304652100768c61b29a6301ab7e241
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-10-03 18:12:30 -0700

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/biyiemmy/alx-low_level_programming/master/0x18-dynamic_libraries/biyiemmy.so
+wget -P /tmp https://github.com/biyiemmy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/biyiemmy.so
 export LD_PRELOAD=/tmp/biyiemmy.so
