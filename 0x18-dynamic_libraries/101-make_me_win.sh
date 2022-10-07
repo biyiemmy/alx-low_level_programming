ef6e28ecfb59cb39d5ad986cdbd4f6fd08608e80
Adebiyi Emmanuel <adebiyiemmanuel11@gmail.com>
2022-10-07 11:06:11 -0700

    update: 101 done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -P /tmp https://github.com/biyiemmy/alx-low_level_programming/raw/master/0x18-dynamic_libraries/biyiemmy.so
+export LD_PRELOAD=/tmp/biyiemmy.so
